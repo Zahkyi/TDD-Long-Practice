22a5075047a9db0fb4b4f5b5c1bc81269e74bf9f
dtitus929 <dave@creativegozone.com>
2022-11-17 19:32:53 -0500

    My Map Solved with Help from Briana

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,18 @@
 function myMap(inputArray, callback) {
-  // Your code here
+  let newArray = [];
+  for (let curVal of inputArray) {
+    newArray.push(callback(curVal));
+  }
+  return newArray
 }
+
+
+// const arr = [1, 2, 3];
+// const callback = (el) => el * 2;
+// let myCallback = myMap(arr, callback)
+// // console.log(myMap(arr, callback)); // prints [2,4,6]
+// // console.log(arr); // prints [1,2,3]
+// console.log(myCallback);
+// console.log(myCallback === [2, 4, 6]);
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,16 @@
-// Your code here
+const chai = require('chai');
+const expect = chai.expect;
+
+const myMap = require('../problems/my-map')
+
+describe('My Map', function () {
+    it('Should operate like map method', function () {
+        // let testArray = [1, 2, 3];
+        // let myCallback = (el) => el * 2;
+
+        expect(myMap([1, 2, 3], (el) => el * 2)).to.eql([2, 4, 6]);
+
+        // expect(testArray).to.equal([1, 2, 3])
+
+    });
+});
